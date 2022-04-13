bc55cefec1b18fd2cc3c88de04cc0dc2da405e3f
abolfzal shamsollahi <abazsh3@gmail.com.com>
2022-04-13 17:58:25 +0430

    added some constants

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const WOEID = 2251945; //where on earth id of tehran
+export const API_BASE_URL = "https://www.metaweather.com/api/";
+export const METAWEATHER_URL = "https://www.metaweather.com/";
